d34a0bdb15040ecc070295c5e0c2ef24543a26cf
Danil gdj <danilguidjou7@gmail.com>
2024-08-14 04:11:25 +0100

    ajout tailwind props

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkPurple: '#1C0B19',
+        frenchGray: '#AAABBC',
+        fawn: "#F7B267"
+      },
+      width: {
+        '300px': '300px',
+      },
+      height:{
+        '20vh': '10vh',
+        '70vh': '82vh',
+        '10vh': '8vh'
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
